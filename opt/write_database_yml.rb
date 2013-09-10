9e07fb75e8240fa2696bb4b4652fdfdb37d6d884
Angelo Grossini <ennegielle@gmail.com>
2013-09-10 17:20:29 +0200

    Allow extra database configuration (eg remote database)

--- opt/write_database_yml.rb
+++ opt/write_database_yml.rb
@@ -2,6 +2,7 @@
 # Replaces the 80-line giant that is database_support.rb
 
 require 'uri'
+require 'json'
 
 puts "Writing config/database.yml from $DATABASE_URL"
 uri = URI.parse ENV['DATABASE_URL']
@@ -25,4 +26,39 @@ File.open(ymlfile, 'w') do |fh|
   password: #{uri.password}
   database: #{database}
 ")
+end
+
+# {name: uri, name1: uri1}
+
+ext = ENV['EXTERNAL_DATABASE_CONFIG'] 
+
+begin
+ext = JSON.parse(ext) if ext
+rescue
+end
+
+if ext.is_a?(Hash)
+  File.open(ymlfile, 'a') do |fh|
+    ext.each do | name, cfg |
+      uri = uri.parse cfg
+      adapter = if uri.scheme == "mysql"
+        "mysql2"
+      else
+        uri.scheme
+      end
+      _, _, database = uri.path.rpartition "/"
+      
+      fh.puts("#{name}:
+        adapter: #{adapter}
+        encoding: utf8
+        pool: 5
+        reconnect: false
+        host: #{uri.host}
+        port: #{uri.port}
+        username: #{uri.user}
+        password: #{uri.password}
+        database: #{database}
+      ")
+    end
+  end
 end
