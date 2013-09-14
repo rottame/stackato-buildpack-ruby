b50cec78482c333d26489afed6abbda9386fd346
Angelo Grossini <ennegielle@gmail.com>
2013-09-15 00:09:02 +0200

    Remove extra database config

--- opt/write_database_yml.rb
+++ opt/write_database_yml.rb
@@ -2,7 +2,6 @@
 # Replaces the 80-line giant that is database_support.rb
 
 require 'uri'
-require 'json'
 
 puts "Writing config/database.yml from $DATABASE_URL"
 uri = URI.parse ENV['DATABASE_URL']
@@ -37,39 +36,4 @@ development:
   password: #{uri.password}
   database: #{database}
 ")
-end
-
-# {name: uri, name1: uri1}
-
-ext = ENV['EXTERNAL_DATABASE_CONFIG'] 
-
-begin
-ext = JSON.parse(ext) if ext
-rescue
-end
-
-if ext.is_a?(Hash)
-  File.open(ymlfile, 'a') do |fh|
-    ext.each do | name, cfg |
-      uri = URI.parse cfg
-      adapter = if uri.scheme == "mysql"
-        "mysql2"
-      else
-        uri.scheme
-      end
-      _, _, database = uri.path.rpartition "/"
-      
-      fh.puts("#{name}:
-        adapter: #{adapter}
-        encoding: utf8
-        pool: 5
-        reconnect: false
-        host: #{uri.host}
-        port: #{uri.port}
-        username: #{uri.user}
-        password: #{uri.password}
-        database: #{database}
-      ")
-    end
-  end
 end
